4cca0d04a162fc1675c05de3002ec96a3bede9c3
dodoburner <100496179+dodoburner@users.noreply.github.com>
2023-03-08 12:58:26 +0100

    Add update and delete note routes

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -32,15 +32,10 @@ export const getNote: RequestHandler = async (req, res, next) => {
   }
 };
 
-interface CreateNoteBody {
-  title?: string;
-  text?: string;
-}
-
 export const createNote: RequestHandler<
   unknown,
   unknown,
-  CreateNoteBody,
+  NoteBody,
   unknown
 > = async (req, res, next) => {
   const { title, text } = req.body;
@@ -57,3 +52,69 @@ export const createNote: RequestHandler<
     next(error);
   }
 };
+
+export const updateNote: RequestHandler<
+  UrlParams,
+  unknown,
+  NoteBody,
+  unknown
+> = async (req, res, next) => {
+  const { noteId } = req.params;
+  const { title, text } = req.body;
+
+  try {
+    if (!title) {
+      throw createHttpError(400, "Note must have a title!");
+    }
+
+    if (!isValidObjectId(noteId)) {
+      throw createHttpError(400, "Note id is not valid!");
+    }
+
+    const note = await Note.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found!");
+    }
+
+    note.title = title;
+    note.text = text;
+
+    const updatedNote = await note.save();
+
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+  const { noteId } = req.params;
+
+  try {
+    if (!isValidObjectId(noteId)) {
+      throw createHttpError(400, "Note id is not valid!");
+    }
+
+    const note = await Note.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found!");
+    }
+
+    await note.remove();
+
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UrlParams {
+  noteId: string;
+}
+
+interface NoteBody {
+  title?: string;
+  text?: string;
+}
