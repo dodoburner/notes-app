bb54dab1cd4781fff2c98fda26bb9433a40eb385
dodoburner <100496179+dodoburner@users.noreply.github.com>
2023-03-08 12:29:07 +0100

    Add http errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { isHttpError } from "http-errors";
 import morgan from "morgan";
 import NotesRoutes from "./routes/notes";
 
@@ -12,15 +13,19 @@ app.use(express.json());
 app.use("/api/notes", NotesRoutes);
 
 app.use((req, res, next) => {
-  next(Error("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMsg = "Unknown error occured";
-  if (error instanceof Error) errorMsg = error.message;
-  res.status(500).json({ error: errorMsg });
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    errorMsg = error.message;
+    statusCode = error.status;
+  }
+  res.status(statusCode).json({ error: errorMsg });
 })
 
 export default app;
--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,4 +1,6 @@
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import { isValidObjectId } from "mongoose";
 import Note from "../models/note";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
@@ -14,18 +16,42 @@ export const getNote: RequestHandler = async (req, res, next) => {
   const { noteId } = req.params;
 
   try {
+    if (!isValidObjectId(noteId)) {
+      throw createHttpError(400, "Note id is not valid!");
+    }
+
     const note = await Note.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found!");
+    }
+
     res.status(200).json(note);
   } catch (error) {
     next(error);
   }
+};
+
+interface CreateNoteBody {
+  title?: string;
+  text?: string;
 }
 
-export const createNote: RequestHandler = async (req, res, next) => {
+export const createNote: RequestHandler<
+  unknown,
+  unknown,
+  CreateNoteBody,
+  unknown
+> = async (req, res, next) => {
   const { title, text } = req.body;
 
   try {
+    if (!title) {
+      throw createHttpError(400, "Note must have a title!");
+    }
+
     const newNote = await Note.create({ title, text });
+
     res.status(201).json(newNote);
   } catch (error) {
     next(error);
